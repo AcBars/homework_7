df5290f379b5b2c34b9fefe09607c3ab3241867d
Artem Mikhailob <mobail_2003@bk.ru>
2022-07-04 11:59:56 +0300

    homework_7 ver.3

---
+++ MyLib/Ex.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLib;
+using System;
+public class Ex
+{
+    public static int [,] GetArray (int m, int n)
+    {
+        int [,] result=new double [m, n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                result[i,j]=new Random().Next(-100,101);
+            }
+        }
+    return result;
+    }
+}
